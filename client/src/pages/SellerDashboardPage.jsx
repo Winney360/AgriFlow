3bfae290ee90055e9965e4e4e3540bc8463f50ad
Winney360 <nkathawinnie94@gmail.com>
2026-03-13 15:31:04 +0300

    feat: update button label and styling for marking products as sold in SellerDashboardPage

--- client/src/pages/SellerDashboardPage.jsx
+++ client/src/pages/SellerDashboardPage.jsx
@@ -324,9 +324,9 @@ export const SellerDashboardPage = () => {
                         </Link>
                         <button 
                           onClick={() => markSold(product._id)}
-                          className="px-3 py-1 text-sm font-semibold text-[#f0a000] hover:bg-[#fffbf0] rounded"
+                          className="px-3 py-1 text-sm font-semibold text-[#20a46b] hover:bg-[#e8f6ef] rounded"
                         >
-                          Pause
+                          Sold
                         </button>
                         <button 
                           onClick={() => remove(product._id)}
